c1ff8d1c8cdcccc303604bf8e6f32a4c33827d5e
Iuliusgh <81573719+Iuliusgh@users.noreply.github.com>
2024-03-05 17:47:10 +0100

    Initial commit

--- TMI-2024-APP/src/views/Tab1Page.vue
+++ TMI-2024-APP/src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Visita-Me</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -11,13 +11,13 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
+      <ion-button href="/tabs/tab2">Start</ion-button>
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
