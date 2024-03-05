1f3f36ea28f9c32073215a817d911ff9ca35b901
Iuliusgh <81573719+Iuliusgh@users.noreply.github.com>
2024-03-05 18:41:49 +0100

    Start button y header, falta background

--- TMI-2024-APP/src/views/Tab1Page.vue
+++ TMI-2024-APP/src/views/Tab1Page.vue
@@ -1,18 +1,19 @@
 <template>
   <ion-page>
-    <ion-header>
+    <ion-header >
       <ion-toolbar>
-        <ion-title>Visita-Me</ion-title>
+        <ion-title class="header-title">Visita-Me</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" class="background-landing">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-button href="/tabs/tab2">Start</ion-button>
-      <ExploreContainer name="Tab 1 page" />
+      <div class="start-button-container">
+        <ion-button href="/tabs/tab2" size="large" class="start-button">Start</ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
