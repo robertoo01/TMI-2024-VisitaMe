0fd2d3fa3c44500315f3cf8a4e1f14a7b2c438b6
Iuliusgh <iuliusgh@ucm.es>
2024-04-08 22:52:54 +0200

    places y photo api funcionando
    hay que ponerlo bonito, pero ya funciona la api

--- TMI-2024-APP/src/views/SiteListPage.vue
+++ TMI-2024-APP/src/views/SiteListPage.vue
@@ -12,54 +12,67 @@
         </ion-toolbar>
       </ion-header>
       <ion-item>
-    <ion-label>Ubicación: Madrid</ion-label>
-    </ion-item>
-    <ion-item>
-    <ion-list :inset="true">
-      <ion-label>Lista de lugares emblemáticos a visitar</ion-label>
-      <ion-item>
-        <ion-label>Palacio Real de Madrid</ion-label>
-        <ion-button @click="push">></ion-button>
+        <ion-label>Ubicación: Madrid</ion-label>
       </ion-item>
       <ion-item>
-        <ion-label>Casa de Campo</ion-label>
+        <ion-list :inset="true" v-if="places.length == 5">
+          <ion-label>Lista de lugares emblemáticos a visitar</ion-label>
+          <ion-item v-for="place in places" :key="place.id">
+              <ion-label>{{ place.name }}</ion-label>
+              <ion-img :src="'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=' + place.photos[0].photo_reference + '&key=AIzaSyAiiWwBeiWGDdBOFCJ480LUHaBju8aneWU'"></ion-img>
+          </ion-item>
+        </ion-list>
       </ion-item>
-      <ion-item>
-        <ion-label>Plaza de España</ion-label>
-      </ion-item>
-      <ion-item>
-        <ion-label>Templo de Debod</ion-label>
-      </ion-item>
-      <ion-item>
-        <ion-label>Plaza Mayor</ion-label>
-      </ion-item>
-    </ion-list>
-  </ion-item>
-  <!--div id="mapContainer" style="height: 400px;"></div-->
-      <!--ul>
-        <li v-for="monument in monuments" :key="monument.place_id">
-          {{ monument.name }}
-        </li>
-      </ul-->
-      
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonList, useIonRouter, IonButton } from '@ionic/vue';
-import { Storage } from '@ionic/storage'; 
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonList, useIonRouter, IonButton, IonImg } from '@ionic/vue';
 import { defineComponent } from 'vue';
 //import { location } from './Tab2Page.vue';
+import axios from "axios"
+
 
 export default defineComponent({
   components: { IonPage },
+  data() {
+    return {
+      lat: 40.4380981,
+      lng: -3.8446862,
+      keyword: "Sitios+emblematicos+de+Madrid",
+      radius: "50000",
+      places: [],
+      imgLinks: [],
+    };
+  },
   setup() {
     const router = useIonRouter();
     const push = () => {
       router.push('/locationDetails');
     };
     return { push };
-  }
+  },
+  mounted() {
+    const URL = "/api/maps/api/place/nearbysearch/json?location="+this.lat+","+this.lng+"&keyword="+this.keyword+"&radius="+this.radius+"&key=AIzaSyAiiWwBeiWGDdBOFCJ480LUHaBju8aneWU";
+    axios.get(URL).then(response => {
+      this.places = response.data.results.slice(0,5);
+      console.log(response.data.results)
+    }).catch(error => {
+      console.log(error.message);
+    });
+
+  },
+  methods: {
+    monumentPhoto(photo_ref:string){
+      const url = "/api/maps/api/place/photo?maxwidth=400&photo_reference=" + photo_ref + "&key=AIzaSyAiiWwBeiWGDdBOFCJ480LUHaBju8aneWU";
+      axios.get(url).then(response => {
+        return response;
+      }).catch(error => {
+      console.log("Error al obtener la foto: " + error.message);
+    });
+    }
+  },
 });
 </script>
--- TMI-2024-APP/vite.config.ts
+++ TMI-2024-APP/vite.config.ts
@@ -5,6 +5,15 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://maps.googleapis.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     vue(),
     legacy()
