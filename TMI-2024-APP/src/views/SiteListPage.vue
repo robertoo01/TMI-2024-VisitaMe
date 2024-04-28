bb41f1ae369a6ed4246a48f79c75c1db8efeab45
Iuliusgh <81573719+Iuliusgh@users.noreply.github.com>
2024-04-28 13:26:33 +0200

    refactor imagenes
    refactorizado el metodo para obtener las imagenes de los sitios y comprobado funcionamiento correcto de medir la distancia entre el lugar de la foto y el lugar del monumento

--- TMI-2024-APP/src/views/SiteListPage.vue
+++ TMI-2024-APP/src/views/SiteListPage.vue
@@ -22,13 +22,13 @@
             <ion-button>{{ index }}</ion-button>
             <ion-popover :trigger="'click-trigger-' + index" trigger-action="click">
               <ion-label>{{ place.name }}
-              <ion-label>UBICACION:<br>{{ place.vicinity }}</br>
-              <ion-label>COORDENADAS: {{place.geometry.location.lat }} , {{ place.geometry.location.lng }}</ion-label>
-                              <ion-img :src="'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=' + place.photos[0].photo_reference + '&key=' + api_key" v-if="place.photos.length > 0"></ion-img>
-            </ion-label>
+                <ion-label>UBICACION:<br>{{ place.vicinity }}</br>
+                  <ion-img :src=imgLinks[index]></ion-img>
+                </ion-label>
               </ion-label>
-              <ion-button @click="takePicture">Foto</ion-button>
-              
+              <ion-button @click="takePicture(index)">Foto</ion-button>
+
+
             </ion-popover>
 
           </ion-item>
@@ -58,8 +58,11 @@ export default defineComponent({
       lng: -3.8446862,
       keyword: "Sitios+emblematicos+de+Madrid",
       radius: "50000",
+      numSites: 5,
       places: [],
       imgLinks: [],
+      siteCoords: [],
+      photoCoords: [],
     };
   },
   setup() {
@@ -72,78 +75,88 @@ export default defineComponent({
   mounted() {
     const URL = "/api/maps/api/place/nearbysearch/json?location=" + this.lat + "," + this.lng + "&keyword=" + this.keyword + "&radius=" + this.radius + "&key=" + API_KEY;
     axios.get(URL).then(response => {
-      this.places = response.data.results.slice(0, 5);
-      console.log(response.data.results)
+      this.places = response.data.results.slice(0, this.numSites);
+      for (let i = 0; i < this.places.length; i++) {
+        this.siteCoords[i] = this.places[i].geometry.location;
+        this.monumentPhoto(this.places[i].photos[0].photo_reference,i);
+      }
+      //console.log(this.imgLinks)
+      //console.log(response.data.results)
     }).catch(error => {
       console.log(error.message);
     });
 
   },
   methods: {
-    monumentPhoto(photo_ref: string) {
+    monumentPhoto(photo_ref: string, index:number) {
       const url = "/api/maps/api/place/photo?maxwidth=400&photo_reference=" + photo_ref + "&key=" + API_KEY;
-      axios.get(url).then(response => {
-        return response;
+      var xhr = new XMLHttpRequest();
+      var vm = this;
+      xhr.open('GET', url, true)
+      xhr.onload = function () {
+        vm.imgLinks[index]=xhr.responseURL.trim();
+      };
+      xhr.send(null);
+
+    },
+    async takePicture(id: number) {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Base64
+      });
+
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      var imageUrl = image.webPath;
+
+      // Can be set to the src of an image now
+      //imageElement.src = imageUrl;
+      const url = "https://vision.googleapis.com/v1/images:annotate?key=" + API_KEY;
+      const request = {
+        requests: [
+          {
+            image: {
+              content: image.base64String
+            },
+            features: [
+              {
+                type: 'LANDMARK_DETECTION'
+              }
+            ]
+          }
+        ]
+      };
+      await axios.post(url, request).then(response => {
+        const coords = response.data.responses[0].landmarkAnnotations[0].locations[0].latLng;
+        this.photoCoords[id] = coords;
+        console.log(this.photoCoords)
+        console.log(this.haversine(this.siteCoords[id],this.photoCoords[id]))
       }).catch(error => {
         console.log("Error al obtener la foto: " + error.message);
       });
     },
-    async takePicture() {
-  const image = await Camera.getPhoto({
-    quality: 90,
-    allowEditing: true,
-    resultType: CameraResultType.Base64
-  });
+    toRadians(degrees) {
+      return degrees * (Math.PI / 180);
+    },
+    haversine(site1, site2) {
+      const R = 6371000; // Radio de la Tierra en metros
+      const phi1 = this.toRadians(site1.lat);
+      const phi2 = this.toRadians(site2.latitude);
+      const deltaPhi = this.toRadians(site2.latitude - site1.lat);
+      const deltaLambda = this.toRadians(site2.longitude - site1.lng);
 
-  // image.webPath will contain a path that can be set as an image src.
-  // You can access the original file using image.path, which can be
-  // passed to the Filesystem API to read the raw data of the image,
-  // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-  var imageUrl = image.webPath;
+      const a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
+        Math.cos(phi1) * Math.cos(phi2) *
+        Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
 
-  // Can be set to the src of an image now
-  //imageElement.src = imageUrl;
-  const url = "https://vision.googleapis.com/v1/images:annotate?key=" + API_KEY;
-  const request = {
-    requests: [
-      {
-        image: {
-          content: image.base64String
-        },
-        features:[
-          {
-            type: 'LANDMARK_DETECTION'
-          }
-        ]
-      }
-    ]
-  };
-  await axios.post(url,request).then(response => {
-    const coords = response.data.responses[0].landmarkAnnotations[0].locations[0].latLng;
-    return coords;
-  }).catch(error => {
-        console.log("Error al obtener la foto: " + error.message);
-      });
-},
-toRadians(degrees) {
-    return degrees * (Math.PI/180);
-},
-haversine(lat1, lon1, lat2, lon2) {
-    const R = 6371000; // Radio de la Tierra en metros
-    const phi1 = this.toRadians(lat1);
-    const phi2 = this.toRadians(lat2);
-    const deltaPhi = this.toRadians(lat2 - lat1);
-    const deltaLambda = this.toRadians(lon2 - lon1);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    const a = Math.sin(deltaPhi/2) * Math.sin(deltaPhi/2) +
-              Math.cos(phi1) * Math.cos(phi2) *
-              Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
-
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-    const distance = R * c;
-    return distance;
-}
+      const distance = R * c;
+      return distance;
+    }
 
   },
 });
